78d9ab817814faef1b266591b35fcb50ec3e09fc
Szona <cmielalicja@gmail.com>
2018-09-14 20:33:48 +0200

    create store

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddlewear, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+const middlewear = [thunk];
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    compose(
+        applyMiddlewear(...middlewear),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+); 
+
+export default store;
