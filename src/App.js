543ec6ced17112bf1d2af80e257e865de69520ab
Szona <cmielalicja@gmail.com>
2018-09-17 13:51:28 +0200

    add func to handle pagination events
    connect  next and prev page functions from actions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import Pokedex from './components/Pokedex';
-
+import Pagination from './components/Pagination';
 import { connect } from 'react-redux';
-import { fetchPokemons } from './actions/dataAction';
+import { fetchPokemons, nextPage, previousPage } from './actions/dataAction';
+
 
 class App extends Component { 
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchPokemons();
+    
   }  
+
+  handlePreviousPage() {
+    this.props.previousPage();
+    this.props.fetchPokemons(this.props.page);
+  }
+
+  handleNextPage() {
+    this.props.nextPage();
+    this.props.fetchPokemons(this.props.page);
+  }
 
   render() {
     return (
         <div className="app">
-          <Pokedex pokemons={this.props.pokemons}/>
+          <Pokedex pokemons={this.props.pokemons} />
+          <Pagination page={this.props.page} 
+          prevPage={() => this.handlePreviousPage()}
+          nextPage={() => this.handleNextPage()}/>
         </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  pokemons: state.data.items
+  pokemons: state.data.pokemons,
+  page: state.data.page
 });
 
-export default connect(mapStateToProps, { fetchPokemons })(App);
+export default connect(mapStateToProps, { fetchPokemons, nextPage, previousPage })(App);
