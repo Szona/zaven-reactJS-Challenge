9110bc399eabb73b5f96983fbb6c6b7d014e2b0d
Szona <cmielalicja@gmail.com>
2018-09-17 13:52:20 +0200

    add on cilck events

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class Pagination extends Component {
+    
     render() {
-        const page = this.props.page;
         return (
             <div>
-                <button >Prev</button>
+                <button onClick={() => this.props.prevPage()}>Prev</button>
                 <div> 
-                    {page} <span>of 13</span>
+                    <span> {this.props.page} </span> of 13
                 </div>
-                <button >Next</button>
+                <button onClick={() => this.props.nextPage()}>Next</button>
             </div>
         )
     }
